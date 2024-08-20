37605441dbb54069a0f02002ecabf020b058718d
muhammed shahil <muhammedshahil65@gmail.com>
2024-08-20 13:52:21 +0530

    second commit

--- ProblemTicketSystem/consumer/views.py
+++ ProblemTicketSystem/consumer/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello welcome to consumers page here we are happy to help you")
+    return render(request,'index/index.html')
