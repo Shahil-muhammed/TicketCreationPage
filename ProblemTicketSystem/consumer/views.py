2751511457b530e48bc03a35a3ec7bf3847d7ad5
muhammed shahil <muhammedshahil65@gmail.com>
2024-08-22 16:50:10 +0530

    5th updation impimented for loop filters

--- ProblemTicketSystem/consumer/views.py
+++ ProblemTicketSystem/consumer/views.py
@@ -2,4 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return render(request,'index/index.html')
+    data = [
+        {
+            'title': 'Track Existing Problem',
+            'description': 'Enter your problem ID to track the status of your issue.',
+            'image': 'search.jpg',
+            'pit': 'Problem ID',
+            'epit': 'Enter Problem ID',
+            'description_field': False,  
+            'button': 'Track Problem'
+        },
+        {
+            'title': 'Create New Problem',
+            'description': 'Describe your issue in detail and submit it to get assistance.',
+            'image': 'Problem.jpg',
+            'pit': 'Problem Title',
+            'epit': 'Enter Title',
+            'description_field': 'Enter description for your issue',  
+            'edescription': 'Describe the issue',  
+            'button': 'Submit Problem'
+        }
+    ]
+
+    return render(request, 'index/index.html', {'data': data})
