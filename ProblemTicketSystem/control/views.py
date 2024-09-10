724184fd3528038e4430f68e288ebf94d66284aa
muhammed shahil <muhammedshahil65@gmail.com>
2024-09-10 15:55:36 +0530

    13th commit || Updated Admin page with welcome message,logout and demo card

--- ProblemTicketSystem/control/views.py
+++ ProblemTicketSystem/control/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.contrib.auth import logout
 
 @login_required  # Ensures the user is logged in
 def SecondAdmin(request):
     if request.user.is_authenticated and request.user.username == 'admin':
-        return HttpResponse("Hi, welcome to the second admin page")
+        username = "Muhammed Shahil KP"
+        return render (request,'Aindex/aindex.html',{'data':username})
     else:
         return HttpResponse("You are not authorized to access this page.", status=403)
